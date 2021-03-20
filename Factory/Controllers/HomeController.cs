7697913c227a2a724f438673a5fb1e57cd9ea3d2
tien96ng <tien96ng@gmail.com>
2021-03-19 20:04:15 -0700

    Create HomeController.cs

---
+++ Factory/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+
+namespace Factory.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly FactoryContext _db;
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
