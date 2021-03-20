3f85ec2243ebccdf2f0828f937e6d8acae41061e
tien96ng <tien96ng@gmail.com>
2021-03-19 20:00:20 -0700

    Fix naming convention for Context.cs

--- Factory/Models/DesignTimeDbContextFactory.cs
+++ Factory/Models/DesignTimeDbContextFactory.cs
@@ -5,21 +5,21 @@ using System.IO;
 
 namespace Factory.Models
 {
-  public class EngineerMachineContextFactory : IDesignTimeDbContextFactory<EngineerMachineContext>
+  public class FactoryContextFactory : IDesignTimeDbContextFactory<FactoryContext>
   {
 
-    EngineerMachineContext IDesignTimeDbContextFactory<EngineerMachineContext>.CreateDbContext(string[] args)
+    FactoryContext IDesignTimeDbContextFactory<FactoryContext>.CreateDbContext(string[] args)
     {
       IConfigurationRoot configuration = new ConfigurationBuilder()
           .SetBasePath(Directory.GetCurrentDirectory())
           .AddJsonFile("appsettings.json")
           .Build();
 
-      var builder = new DbContextOptionsBuilder<EngineerMachineContext>();
+      var builder = new DbContextOptionsBuilder<FactoryContext>();
 
       builder.UseMySql(configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(configuration["ConnectionStrings:DefaultConnection"]));
 
-      return new EngineerMachineContext(builder.Options);
+      return new FactoryContext(builder.Options);
     }
   }
 }
--- Factory/Models/FactoryContext.cs
+++ Factory/Models/FactoryContext.cs
@@ -2,13 +2,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Factory.Models
 {
-  public class EngineerMachineContext : DbContext
+  public class FactoryContext : DbContext
   {
     public virtual DbSet<Engineer> Engineers { get; set; }
     public DbSet<Machine> Machines { get; set; }
     public DbSet<EngineerMachine> EngineerMachine { get; set; }
 
-    public EngineerMachineContext(DbContextOptions options) : base(options) { }
+    public FactoryContext(DbContextOptions options) : base(options) { }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
