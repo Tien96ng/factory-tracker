f833bfc3bc627ba0ddb546d9161ab549acc53a84
tien96ng <tien96ng@gmail.com>
2021-03-21 20:34:09 -0700

    Create Create() Route for Engineer POST and GET.

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -1,8 +1,9 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
 using System.Collections.Generic;
 using System.Linq;
-using Factory.Models;
 
 namespace Factory.Controllers
 {
@@ -18,6 +19,28 @@ namespace Factory.Controllers
     {
       List<Engineer> model = _db.Engineers.ToList();
       return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Engineer engineer, int machineId)
+    {
+      bool matches = _db.EngineerMachine.Any(x => x.EngineerId == engineer.EngineerId && x.MachineId == machineId);
+      if(!matches) {
+        _db.Engineers.Add(engineer);
+        _db.SaveChanges();
+        if(machineId != 0)
+        {
+          _db.EngineerMachine.Add(new EngineerMachine() { EngineerId = engineer.EngineerId, MachineId = machineId });
+        }
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Index");
     }
   }
 }
